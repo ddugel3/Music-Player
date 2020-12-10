5b83c058578bedbdab7c51dce91cf9b9a46e08f1
ddugel3 <ddugel3@gmail.com>
2020-12-10 21:46:05 +0900

    Organize files
    Organize files

--- home 11.py
+++ home 11.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import (QWidget, QPushButton, QHBoxLayout, QFrame, QTableWidget,QHeaderView, QTableWidgetItem, QVBoxLayout, QApplication,
-                             QLabel,QLineEdit, QGroupBox, QFileDialog,QStyledItemDelegate)
+                             QLabel,QLineEdit, QGroupBox, QFileDialog, QMainWindow, QStyledItemDelegate)
 from PyQt5 import QtCore, QtGui,QtWidgets
 import sys
 
@@ -20,6 +20,7 @@ class HTMLDelegate(QStyledItemDelegate):
         options = QtWidgets.QStyleOptionViewItem(option)
         self.initStyleOption(options, index)
         res = ""
+
         color = QtGui.QColor("orange")
         if substring:
             substrings = options.text.split(substring)
@@ -57,10 +58,6 @@ class HTMLDelegate(QStyledItemDelegate):
         if index.column() != 0:
             textRect.adjust(5,0,0,0,0)
 
-        thefuckyourshitup_constant = 1
-        margin = (option.rect.height() - options.fontMetrics.height()) // 2
-        margin = margin - thefuckyourshitup_constant
-        textRect.setTop(textRect.top() + margin)
 
         painter.translate(textRect.topLeft())
         painter.setClipRect(textRect.translated(-textRect.topLeft()))
@@ -81,11 +78,14 @@ class Mp3Player(QWidget):
     def initUI(self):
 
         #button,title...
-        name = QLabel(" 멋진이름 ") #mp3 이름(미정)
+        self.logo = QPushButton() #추가버튼
+        self.logo.setIcon(QtGui.QIcon('icon/logo.png'))
+        self.logo.setIconSize(QtCore.QSize(50,50))
+
+        name = QLabel(" H-music ") #mp3 이름(미정)
         name.setFont(QtGui.QFont("궁서",30,QtGui.QFont.Bold))
         name.setStyleSheet("Color : olive")
         self.playlist=[]
-        self.selectedList = [0]
 
         self.search = QLineEdit() #검색
         font = self.search.font()
@@ -106,25 +106,23 @@ class Mp3Player(QWidget):
         closebutton.setIconSize(QtCore.QSize(50,50))
         closebutton.clicked.connect(app.quit)
 
-        upbutton = QPushButton() #위버튼
-        upbutton.setIcon(QtGui.QIcon('icon/up.png'))
-        upbutton.setIconSize(QtCore.QSize(50,50))
-
-        downbutton = QPushButton() #아래버튼
-        downbutton.setIcon(QtGui.QIcon('icon/down.png'))
-        downbutton.setIconSize(QtCore.QSize(50,50))
-
         newbutton = QPushButton() #추가버튼
         newbutton.setIcon(QtGui.QIcon('icon/new.png'))
         newbutton.setIconSize(QtCore.QSize(50,50))
         newbutton.clicked.connect(self.addList)
+
+        choosebutton = QPushButton()#리스트선택 후 넘어가기 버튼
+        choosebutton.setIcon(QtGui.QIcon('icon/choose.png'))
+        choosebutton.setIconSize(QtCore.QSize(50,50))
+        choosebutton.clicked.connect(self.window2)
+
         #Layout
         v1box = QVBoxLayout() #playlist쪽 레이아웃
         v2box = QVBoxLayout() #버튼 모음 레이아웃
 
         # mp3 이름, 검색 창 레이아웃
         h2box = QHBoxLayout()
-        h2box.addWidget(name)
+        h2box.addWidget(self.logo)
         h2box.addWidget(self.search)
         h2box.addWidget(self.searchbutton)
 
@@ -140,14 +138,16 @@ class Mp3Player(QWidget):
         playlist = QGroupBox("Play List")  # playlist를 담을곳
         v1box.addWidget(playlist)
 
-        self.table =QTableWidget(0,1,self)
+        self.table =QTableWidget()
+        self.table.setRowCount(20)
+        self.table.setColumnCount(1)
+        self.setTableData()
         header = self.table.horizontalHeader()
         header.setFrameStyle(QFrame.Plain)
         header.setLineWidth(1)
         self.table.setHorizontalHeader(header)
         self.table.setHorizontalHeaderLabels(['노래목록'])
         self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        self.table.itemSelectionChanged.connect(self.tableChanged)
         self.table.setItemDelegate(HTMLDelegate(self.table))
         box.addWidget(self.table)
 
@@ -161,12 +161,9 @@ class Mp3Player(QWidget):
         v2_1box.addWidget(closebutton)
         v2_1box.addStretch(1)
 
-        v2_2box.addWidget(upbutton)
-        v2_2box.addWidget(downbutton)
-
         v2_3box.addStretch(1)
         v2_3box.addWidget(newbutton)
-
+        v2_3box.addWidget(choosebutton)
 
         Main=QHBoxLayout()
         Main.addLayout(v1box)
@@ -178,32 +175,45 @@ class Mp3Player(QWidget):
         self.setGeometry(500, 200, 1000, 700)
         self.show()
 
-    def tableChanged(self):
-        self.selectedList.clear()
-        for item in self.table.selectedIndexes():
-            self.selectedList.append(item.row())
-
-        self.selectedList = list(set(self.selectedList))
-
+    def setTableData(self):
+        self.table.setItem(0,0,QTableWidgetItem("Make a wish.mp3"))
+        self.table.setItem(0,1,QTableWidgetItem("cheer up.mp3"))
+        self.table.setItem(0,2,QTableWidgetItem("snowman.mp3"))
+        self.table.setItem(0,3,QTableWidgetItem("santa tell me.mp3"))
+        self.table.setItem(0,4,QTableWidgetItem("text me merry chirstmas.mp3"))
+        self.table.setItem(0,5,QTableWidgetItem("black out.mp3"))
+        self.table.setItem(0,6,QTableWidgetItem("perhaps love.mp3"))
+        self.table.setItem(0,7,QTableWidgetItem("fly.mp3"))
+        self.table.setItem(0,8,QTableWidgetItem("what are we.mp3"))
+        self.table.setItem(0,9,QTableWidgetItem("thunder.mp3"))
+        self.table.setItem(0,10,QTableWidgetItem("adios.mp3"))
+        self.table.setItem(0,11,QTableWidgetItem("RBB.mp3"))
+        self.table.setItem(0,12,QTableWidgetItem("aside.mp3"))
+        self.table.setItem(0,13,QTableWidgetItem("hug.mp3"))
+        self.table.setItem(0,14,QTableWidgetItem("savage love.mp3"))
+        self.table.setItem(0,15,QTableWidgetItem("my page.mp3"))
+        self.table.setItem(0,16,QTableWidgetItem("square.mp3"))
+        self.table.setItem(0,17,QTableWidgetItem("welcome to my playground.mp3"))
+        self.table.setItem(0,18,QTableWidgetItem("i can't stop me.mp3"))
+        self.table.setItem(0,19,QTableWidgetItem("what is love?.mp3"))
+        self.table.setItem(0,20,QTableWidgetItem("solo.mp3"))
 
     def addList(self):
-        files = QFileDialog.getOpenFileNames(self,'Select one or more files to open','','Sound (*.mp3 *.wav *.ogg *.flac *.wma)')
-        cnt= len(files[0])
+        self.files = QFileDialog.getOpenFileNames(self,'Select one or more files to open','','Sound (*.mp3 *.wav *.ogg *.flac *.wma)')
+        Str =str(self.files[0]).split("/")
+        Str1=Str[len(Str) -1].split(("."))
+        self.playlist.append(Str1[1])
+        cnt= len(self.files[0])
         row= self.table.rowCount()
         self.table.setRowCount(row + cnt)
         for i in range(row, row + cnt):
-            self.table.setItem(i,0,QTableWidgetItem(files[0][i-row]))
-
+            self.table.setItem(i,0,QTableWidgetItem(self.files[0][i-row]))
         self.createPlaylist()
 
     def createPlaylist(self):
         self.playlist.clear()
-        for i in range(self.table.rowCount()):
-            self.playlist.append(self.table.item(i,0).text())
-            str= (self.table.item(i,0).text()).split("home/been/문서/GitHub/AD-Project/MUSIC/")
-            self.playlist.append(str[1])
-            print(str[1])
-
+        self.playlist.append(self.files[0])
+        print(self.playlist)
 
     def searchkeyword(self):
         text = self.search.text()
@@ -213,6 +223,15 @@ class Mp3Player(QWidget):
         for item in allitems:
             item.setData(QtCore.Qt.UserRole, text if item in selected_items else None)
 
+    def window2(self):
+        self.w = Window2()
+        self.w.show()
+        self.hide()
+#///////////////////////////////////// main ///////////////////////////////////
+class Window2(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Window22222")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import sys
+import threading
+import time
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
-from PyQt5 import QtCore,QtGui
+from PyQt5 import QtGui
+from PyQt5 import QtCore
 from PyQt5.QtMultimedia import QMediaPlaylist, QMediaPlayer, QMediaContent
-#from main_player import *
 
-from main_player_2 import *
+#from home_11 import *
+
+
 #volume에서 handle 크기 조정
 class SliderProxyStyle(QProxyStyle):
     def pixelMetric(self, metric, option, widget):
@@ -15,43 +19,62 @@ class SliderProxyStyle(QProxyStyle):
             return 40
         return super().pixelMetric(metric, option, widget)
 
-        
+
 class Main(QWidget):
 
     def __init__(self):
         super().__init__()
-        #self.player = MPlayer(self)
-        #self.player = Musicplay(self)
+        self.showplaylist = QDialog()
+        self.currentidx = 0
         self.playlist = QMediaPlaylist()
-        self.url = QUrl.fromLocalFile('../AD-Project/music/Piano-melody_2.mp3')
-        self.playlist.addMedia(QMediaContent(self.url))
+        self.list = ['../AD-Project/music/stay.wav','../AD-Project/music/잠이 오질 않네요.wav',
+                     '../AD-Project/music/Piano-melody_2.mp3','../AD-Project/music/STAY.wav',
+                     '../AD-Project/music/Piano-melody_1.wav']
+        self.a = QUrl.fromLocalFile('../AD-Project/music/stay-PostMalon.wav')
+        self.b = QUrl.fromLocalFile('../AD-Project/music/잠이 오질 않네요-장범준.wav')
+        self.c = QUrl.fromLocalFile('../AD-Project/music/Piano-melody_2.mp3')
+        self.d = QUrl.fromLocalFile('../AD-Project/music/STAY-BLACKPINK.wav')
+        self.e = QUrl.fromLocalFile('../AD-Project/music/Piano-melody_1.wav')
+        self.playlist.addMedia(QMediaContent(self.a))
+        self.playlist.addMedia(QMediaContent(self.b))
+        self.playlist.addMedia(QMediaContent(self.c))
+        self.playlist.addMedia(QMediaContent(self.d))
+        self.playlist.addMedia(QMediaContent(self.e))
         self.player = QMediaPlayer()
         self.player.setPlaylist(self.playlist)
+        self.count = 0
+        self.bartimer = 0
         self.Play()
-        #self.playlist = []
-        #self.selectedList = [0]
-        #self.playOption = QMediaPlaylist.Sequential
+
+
+
+
 
 
 
     def Play(self):
         #button,screen ...
-        screen = QGroupBox() #그래프??가 들어갈 공간
+        self.screen = QGroupBox()
+
 
         self.title = QLineEdit('') #title
         self.title.setReadOnly(True)
-        self.title.setMaxLength(50)
+        self.title.setMaxLength(1000)
         self.title.setFont(QtGui.QFont("Arial", 20, QtGui.QFont.Bold))
 
+
+
         # repeat button
-        replaybutton = QPushButton()
-        replaybutton.setIcon(QtGui.QIcon('../AD-Project/icon/repeat.png'))
-        replaybutton.setIconSize(QtCore.QSize(35,35))
+        self.replaybutton = QPushButton()
+        self.replaybutton.setIcon(QtGui.QIcon('../AD-Project/icon/repeatone.png'))
+        self.replaybutton.setIconSize(QtCore.QSize(35,35))
+        self.replaybutton.clicked.connect(self.currretloop)
 
         # random button
-        randombutton = QPushButton()
-        randombutton.setIcon(QtGui.QIcon('../AD-Project/icon/shuffle.png'))
-        randombutton.setIconSize(QtCore.QSize(35,35))
+        self.randombutton = QPushButton()
+        self.randombutton.setIcon(QtGui.QIcon('../AD-Project/icon/repeat.png'))
+        self.randombutton.setIconSize(QtCore.QSize(35,35))
+        self.randombutton.clicked.connect(self.loop)
 
         # playbar 재생정
         self.playbar = QProgressBar(self)
@@ -73,20 +96,24 @@ class Main(QWidget):
 
 
         #next button
-        nextbutton = QPushButton()
-        nextbutton.setIcon(QtGui.QIcon('../AD-Project/icon/next.png'))
-        nextbutton.setIconSize(QtCore.QSize(80,60))
+        self.nextbutton = QPushButton()
+        self.nextbutton.setIcon(QtGui.QIcon('../AD-Project/icon/next.png'))
+        self.nextbutton.setIconSize(QtCore.QSize(80,60))
+        self.nextbutton.clicked.connect(self.nextClicked)
+
 
         #back button
-        backbutton = QPushButton()
-        backbutton.setIcon(QtGui.QIcon('../AD-Project/icon/back.png'))
-        backbutton.setIconSize(QtCore.QSize(80,60))
+        self.backbutton = QPushButton()
+        self.backbutton.setIcon(QtGui.QIcon('../AD-Project/icon/back.png'))
+        self.backbutton.setIconSize(QtCore.QSize(80,60))
+        self.backbutton.clicked.connect(self.prevClicked)
+
 
         #out button
         outbutton = QPushButton()
         outbutton.setIcon(QtGui.QIcon('../AD-Project/icon/out.png'))
         outbutton.setIconSize(QtCore.QSize(50,50))
-        outbutton.clicked.connect(self.Title)
+        outbutton.clicked.connect(self.Out)
 
         # volume
         self.volume = QSlider(QtCore.Qt.Vertical)
@@ -99,9 +126,10 @@ class Main(QWidget):
         self.volume.valueChanged[int].connect(self.volumeChanged)
 
         # lyricsbutton
-        lyricsbutton = QPushButton()
-        lyricsbutton.setIcon(QtGui.QIcon('../AD-Project/icon/lyrics.png'))
-        lyricsbutton.setIconSize(QtCore.QSize(50,50))
+        self.lyricsbutton = QPushButton()
+        self.lyricsbutton.setIcon(QtGui.QIcon('../AD-Project/icon/lyrics.png'))
+        self.lyricsbutton.setIconSize(QtCore.QSize(50,50))
+        self.lyricsbutton.clicked.connect(self.ShowPLayList)
 
         #layout
         vv1box = QVBoxLayout()
@@ -114,7 +142,7 @@ class Main(QWidget):
 
         #screen 띄우는 위치
         vv1box.addLayout(vv1_v1box)
-        vv1_v1box.addWidget(screen)
+        vv1_v1box.addWidget(self.screen)
 
         #곡제목 위치
         vv1box.addLayout(vv1_h1box)
@@ -124,22 +152,22 @@ class Main(QWidget):
 
         #replay, playbar, random 버튼 위치
         vv1box.addLayout(vv1_h2box)
-        vv1_h2box.addWidget(replaybutton)
+        vv1_h2box.addWidget(self.replaybutton)
         vv1_h2box.addWidget(self.playbar)
-        vv1_h2box.addWidget(randombutton)
+        vv1_h2box.addWidget(self.randombutton)
 
         #back,play,next 버튼
         vv1box.addLayout(vv1_h3box)
         vv1_h3box.addStretch(1)
-        vv1_h3box.addWidget(backbutton)
+        vv1_h3box.addWidget(self.backbutton)
         vv1_h3box.addWidget(self.playbutton)
-        vv1_h3box.addWidget(nextbutton)
+        vv1_h3box.addWidget(self.nextbutton)
         vv1_h3box.addStretch(1)
 
         #out, volume, lyrics 버튼
         vv2box.addWidget(outbutton)
         vv2box.addWidget(self.volume)
-        vv2box.addWidget(lyricsbutton)
+        vv2box.addWidget(self.lyricsbutton)
 
         Mainlayout = QHBoxLayout()
         Mainlayout.addLayout(vv1box)
@@ -151,12 +179,18 @@ class Main(QWidget):
         self.setGeometry(500, 200, 1000, 700)
         self.show()
 
-    def Title(self):
-        self.files = QFileDialog.getOpenFileNames(self, 'Select one or more files to open', '','Sound (*.mp3 *.wav *.ogg *.flac *.wma)')
-        Str = str(self.files[0]).split("/")
+
+
+
+    def Out(self):
+        self.close()
+
+
+    def TTitle(self,idx):
+        Str = str(self.list[idx]).split("/")
         Str1 = Str[len(Str) - 1].split('.')
         self.title.setText('{}'.format(Str1[0]))
-        #self.createPlaylist()
+
 
     def playClicked(self):
         if self.player.state() == QMediaPlayer.PlayingState:
@@ -166,27 +200,100 @@ class Main(QWidget):
             self.playbutton.setIconSize(QtCore.QSize(60, 60))
         else:
             self.player.play()
-            self.timer.start(100,self)
+            self.timer.start(100, self)
             self.playbutton.setIcon(QtGui.QIcon('../AD-Project/icon/stop.png'))
             self.playbutton.setIconSize(QtCore.QSize(60, 60))
+            self.TTitle(self.currentidx)
+
+    def nextClicked(self):
+        self.step = 0
+        self.currentidx +=1
+        self.TTitle(self.currentidx)
+        if self.player.state() == QMediaPlayer.PlayingState:
+            self.timer.start(100, self)
+        self.playlist.next()
+
+
+    def prevClicked(self):
+        self.step = 0
+        self.currentidx -= 1
+        self.TTitle(self.currentidx)
+        if self.player.state() == QMediaPlayer.PlayingState:
+            self.timer.start(100, self)
+        self.playlist.previous()
 
     def volumeChanged(self):
         self.player.setVolume(self.volume.value())
 
-    def createPlaylist(self):
-        self.playlist.clear()
-        self.playlist.append(self.files[0])
-        print(self.playlist)
-
 
 
     def timerEvent(self, e):
-        if self.step >= 1000:
-            self.timer.stop() #재생바 종료
+        self.step = self.step + 1
+        self.playbar.setValue(self.step)
+        if self.step>=1000 :
+            self.step = 0 #재생바 종료
+            self.timer.stop()
             return
 
-        self.step = self.step + 1
-        self.playbar.setValue(self.step) #재생바의 재생정도를 step값을 기준으로 함.
+
+
+         #재생바의 재생정도를 step값을 기준으로 함.
+
+    def ShowPLayList(self):
+
+
+        List = ['stay-PostMalon', '잠이 오질 않네요-장범준','Piano-melody_2', 'STAY-BLACKPINK','Piano-melody_1']
+
+        for (i,j) in zip (List,range(10,110,20)):
+            a = QLabel(i, self.showplaylist)
+            a.move(10, j)
+
+        label = QLabel(self.showplaylist)
+        label.move(0, 110)
+        label.resize(100, 100)
+
+
+
+        self.showplaylist.setWindowTitle('PlayLIst')
+        self.showplaylist.setWindowModality(Qt.ApplicationModal)
+        self.showplaylist.setGeometry(1500, 400, 300, 200)
+        self.showplaylist.show()
+
+
+
+
+
+
+    def closed(self,state):
+        self.label.setVisible(state != Qt.Unchecked)
+
+    def playbartimer(self,a):
+        self.bartimer += 1
+        tr = threading.Timer(1, self.playbartimer, args=[a])
+        tr.start()
+
+        print(self.bartimer)
+        if self.bartimer == 10:
+            self.timer.stop()
+            tr.cancel()
+
+    def currretloop(self):
+        self.playlist.setPlaybackMode(QMediaPlaylist.CurrentItemInLoop)
+
+    def loop(self):
+        self.playlist.setPlaybackMode(QMediaPlaylist.Loop)
+
+    """def playbarstart(self):
+        self.ccount = 0
+        self.c = 0
+        while self.ccount <= 10:
+            self.ccount +=1
+            self.c = self.ccount
+            time.sleep(1)
+            self.playbar.setValue(self.ccount)
+    def playbarstop(self):
+        self.playbar.setValue()"""
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
